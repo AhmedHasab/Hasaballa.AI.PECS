96bba0d3ea74312b38f792b6f4859a50123bfce8
AhmedHasab <namozg50@gmail.com>
2026-06-14 17:30:00 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+const SUPABASE_URL =
+"https://oivtndqstklnsxwherpp.supabase.co";
 
+const SUPABASE_KEY =
+"sb_publishable_8F2wYjRCveireb8x-JOt0A_HyjnkAZB";
+
+const supabase =
+window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
